ddd209467962085f04ea00a6f4ff5729f0f6bda5
Vítor Paixão <153404890+Paixonite@users.noreply.github.com>
2024-08-11 00:15:15 -0300

    Update Evento.java

--- src/TP - 01 2Etapa/Evento/Evento.java
+++ src/TP - 01 2Etapa/Evento/Evento.java
@@ -9,13 +9,16 @@ public abstract class Evento {
     protected double preco;
     protected int qtdIngressos;
 
-    public Evento(String nome, int[] data, String local, double preco) { 
+    public Evento(String nome, int[] data, String local, double preco) {
         this.nome = nome;
         this.data = LocalDateTime.of(data[2], data[1], data[0], data[3], data[4]); // [DD,MM,AA,hh,mm]
-        this.local = local; 
+        this.local = local;
         this.preco = preco;
     }
 
-    
+    public String getNome() {
+        return nome;
+    }
 
+    public abstract String toString();
 }
