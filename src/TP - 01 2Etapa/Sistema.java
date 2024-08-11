6458aeeebad39e80af137fbef9aa037ce5a2d03d
Vítor Paixão <153404890+Paixonite@users.noreply.github.com>
2024-08-11 00:13:12 -0300

    Update Sistema.java

--- src/TP - 01 2Etapa/Sistema.java
+++ src/TP - 01 2Etapa/Sistema.java
@@ -1,102 +1,145 @@
-import java.time.LocalDateTime;
 import java.util.*;
 
 import Evento.Evento;
 import Evento.Tipos.Concerto;
 import Evento.Tipos.Teatro;
 import Evento.Tipos.Filme;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+public class Sistema{
 
-
-
-public class Sistema{           
-
-    //scanner do sistema
-    public Scanner sc = new Scanner(System.in);
-
-    public void cadastrar_eventos()
-    {
-
-        Object tipoEvento;
-        String nome_evento;
-        String local_evento;
-        Date data_evento;
-        float preco_ingresso;
-        String opcao;
-
-
-        JOptionPane.showMessageDialog(null, "Vamos comecar o cadastro de eventos!!!");
-        JOptionPane.showMessageDialog(null, "Qual tipo de evento voce ira cadastrar\nEscolha as opcoes a seguir:\n[c] - concerto \n [f] - filme \n [t] - teatro");
-        String [] itens = {"Concerto" , "Filme" , "Teatro"}; 
-        tipoEvento = JOptionPane.showInputDialog(null, "Escolha um dos eventos", "Opcoes",JOptionPane.INFORMATION_MESSAGE, null,itens, itens [0]); 
-    
-        opcao = tipoEvento.toString();
-       
-        nome_evento = JOptionPane.showInputDialog("Digite o nome do evento : ");
-
-        
-        local_evento =  JOptionPane.showInputDialog("Digite o Local do evento : ");
-
-       
-        preco_ingresso =  Float.parseFloat(JOptionPane.showInputDialog("Digite o preço do ingresso"));
-
-        
-        switch (opcao) {
-            case "Concerto":
-
-                Evento concerto = new Concerto(nome_evento, null, local_evento, preco_ingresso);
-               
-
-                break;
-
-            case "Filme":
-                Evento filme = new Filme(nome_evento, null, local_evento, preco_ingresso);
-
-                break;
-
-            case "Teatro":
-                Evento teatro = new Teatro(nome_evento, null, local_evento, preco_ingresso);
-                break;
-            default:
-            JOptionPane.showMessageDialog(null, "Opcao invalida","Alerta de uso !!!", JOptionPane.ERROR_MESSAGE);
-                break;
-        }
-    }
-    public static void main(String[] args) 
-    {
-        int opcao;
+    public static void main(String[] args) {
         Sistema bilheteria = new Sistema();
-        Date d = new Date();
-        
-        //area do funcionario fazer a manuntencao do sistema
-        JFrame frame = new JFrame("");
-        System.out.println(d);
-        JOptionPane.showMessageDialog(null, "Olá !!! Seja Bem Vindo a nossa BILHETERIA!!!");
-        do{
-            //menu simples + escolha de opcao
-            JOptionPane.showMessageDialog(null, "Area do Funcionario");
-            JOptionPane.showMessageDialog(null, "[0] - Para Encerrar o sistema\n[1] - Para o Cadastro de Eventos\n[2] - Para a Venda de Ingressos\n[3] - Para a Mostrar Eventos\n[4] - Para Mostrar Ingressos\n[5] - Para mostrar o Relatorio de receitas");
-            opcao = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma opcao"));
 
-          
+        Evento[] eventos = new Evento[15]; // Atualmente, é um array estático
+        int numEventos = 0; // Índice do array eventos
+
+        JOptionPane.showMessageDialog(null, "Olá!!! Seja bem-vindo a nossa BILHETERIA!!!");
+
+        boolean sair = false;
+        do{
+            String[] itens = {"Cadastrar eventos", "Comprar ingressos", "Mostrar eventos", "Mostrar ingressos", "Relatorio de receitas", "Encerrar o sistema"};
+            String opcao = JOptionPane.showInputDialog(null, "Escolha uma ação", "Bilheteria", JOptionPane.INFORMATION_MESSAGE, null, itens, itens[0]).toString();
 
             switch (opcao) {
-                case 0:
-                    JOptionPane.showMessageDialog(null, "Encerrando");
+                case "Cadastrar eventos":
+                    eventos[numEventos] = bilheteria.cadastrarEvento();
+                    numEventos++;
                     break;
 
-                case 1:
-                    bilheteria.cadastrar_eventos();
+                case "Comprar ingressos":
+                    bilheteria.comprarIngressos(eventos, numEventos);
                     break;
-                default:
-                JOptionPane.showMessageDialog(null, "Opcao invalida","Alerta de uso !!!", JOptionPane.ERROR_MESSAGE);
+
+                case "Mostrar eventos":
+                    bilheteria.mostrarEventos(eventos, numEventos);
+                    break;
+
+                case "Mostrar ingressos":
+                    bilheteria.mostrarIngressos(eventos, numEventos);
+                    break;
+
+                case "Relatorio de receitas":
+                    bilheteria.relatorioDeReceitas(eventos, numEventos);
+                    break;
+
+                case "Encerrar o sistema":
+                    JOptionPane.showMessageDialog(null, "Encerrando...");
+                    sair = true;
                     break;
             }
 
-        }while(opcao != 0);
-        
+        }while(sair == false);
+    }
+
+    Evento cadastrarEvento(){
+        String tipoEvento;
+        String nomeEvento;
+        String localEvento;
+        Date dataEvento;
+        float precoIngresso;
+
+        String [] itens = {"Concerto", "Filme", "Teatro"};
+        tipoEvento = JOptionPane.showInputDialog(null, "Escolha o tipo de evento a ser cadastrado", "Cadastro", JOptionPane.INFORMATION_MESSAGE, null, itens, itens[0]).toString();
+        nomeEvento = JOptionPane.showInputDialog("Digite o nome do evento :");
+        localEvento = JOptionPane.showInputDialog("Digite o local do evento :");
+        do {
+            precoIngresso = Float.parseFloat(JOptionPane.showInputDialog("Digite o preço do ingresso:"));
+        } while (precoIngresso < 0);
+
+        int[] data = {10,8,24,23,59}; //Solução provisória pra data. Temos que transformar um input do usuario pro formato DD/MM/AA/hh/mm (int[4])
+
+        Evento e;
+        switch (tipoEvento) {
+            case "Concerto":
+                e = new Concerto(nomeEvento, data, localEvento, precoIngresso);
+                break;
+
+            case "Filme":
+                e = new Filme(nomeEvento, data, localEvento, precoIngresso);
+                break;
+
+            case "Teatro":
+                e = new Teatro(nomeEvento, data, localEvento, precoIngresso);
+                break;
+
+            default: // Esse caso nunca acontece, está aqui para burlar o java
+                e = new Concerto(nomeEvento, data, localEvento, precoIngresso);
+                break;
+        }
+        return e;
+    }
+
+    void mostrarEventos(Evento[] eventos, int numEventos){
+        String s = "";
+        for (int i = 0; i < numEventos; i++){
+            s += eventos[i].toString(); // Pequeno erro na formatação da data e muitas casas decimais em preços quebrados
+            if (i == numEventos - 1) // Tira o último espaço
+                break;
+            s += "\n\n";
+        }
+        JOptionPane.showMessageDialog(null, s);
+    }
+
+    void comprarIngressos(Evento[] eventos, int numEventos){
+        String[] itens = new String[numEventos];
+        for(int i = 0; i < numEventos; i++) {
+            itens[i] = eventos[i].getNome();
+        }
+
+        JOptionPane.showInputDialog(null, "Selecione o evento", "Compra de ingressos", JOptionPane.INFORMATION_MESSAGE, null, itens, itens[0]);
+        // Tem que achar um jeito de transformar esse input no índice de eventos[] pra podermos usar nacompra de ingressos
+
+        // Compra ingressos aqui
+    }
+
+    void mostrarIngressos(Evento[] eventos, int numEventos){
+        String s = "";
+        for (int i = 0; i < numEventos; i++){
+            s += eventos[i].getNome();
+
+            // A quantidade e tipo de cada ingresso disponível vai aqui
+
+            if (i == numEventos - 1) // Tira o último espaço
+                break;
+            s += "\n\n";
+        }
+        JOptionPane.showMessageDialog(null, s);
+    }
+
+    void relatorioDeReceitas(Evento[] eventos, int numEventos){
+        String s = "";
+        double total = 0;
+        for (int i = 0; i < numEventos; i++){
+            s += eventos[i].getNome();
+
+            // A receita gerada por cada evento vai aqui
+            // total += evento[i].calculaReceita();
+
+            s += "\n\n";
+        }
+        s += "Total de receita acumulada: R$" + total;
+        JOptionPane.showMessageDialog(null, s);
     }
 
 }
