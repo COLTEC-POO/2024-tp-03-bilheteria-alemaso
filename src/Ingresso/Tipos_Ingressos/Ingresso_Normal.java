76e87257f87d58ab3e517858178466aef6a18850
Vítor Paixão <153404890+Paixonite@users.noreply.github.com>
2024-08-12 16:19:01 -0300

    Update Ingresso_Normal.java

--- src/Ingresso/Tipos_Ingressos/Ingresso_Normal.java
+++ src/Ingresso/Tipos_Ingressos/Ingresso_Normal.java
@@ -5,18 +5,19 @@ import Ingresso.*;
 
 public class Ingresso_Normal extends Ingresso {
 
-    protected float preco;
-    protected char tipo;
-
-    public Ingresso_Normal(Date dataVenda , float preco , char type)
+    public Ingresso_Normal(Date dataVenda , double preco)
     {
-        super(dataVenda);
-        this.preco = preco;
-        this.tipo = type;
+        super(dataVenda, preco);
     }
 
     @Override
-    public int calculaMaximo() {
-        return 0;
+    public double calculaReceita() {
+        return preco;
+    }
+
+    @Override
+    public String exibeExtrato() {
+        String s = "Ingresso Normal - " + dataVenda + " - R$" + calculaReceita();
+        return s;
     }
 }
